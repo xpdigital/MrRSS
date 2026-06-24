705991f177efb0cea383ccd11fac393a10d86fab
xpdigital <agentmao@qq.com>
2026-06-24 05:11:01 +0000

    chore: bump version to 1.3.23-mod.3

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,4 +2,4 @@ package version
 
 // Version is the current application version.
 // This fork uses the upstream version plus a "-mod.N" suffix.
-const Version = "1.3.23-mod.2"
+const Version = "1.3.23-mod.3"
